2c2b387aeef0a170a27368bf73fe8c25e96934bd
Silmara Eliza <silmara.eliza@gmail.com>
2022-09-14 20:24:01 -0300

    feat: adding coverage test

---
+++ README.md
@@ -0,0 +1,7 @@
+# Comandos usados para testar o coverage
+
+- python3 statement.py 5
+- coverage report -m
+- coverage run statement.py 5
+- coverage html -d coveragereport
+- coverage run --branch branch.py 1
---
+++ branch.py
@@ -0,0 +1,12 @@
+import sys
+
+def BranchCoverageDemo(n):
+    if n > 0:
+        x = n
+    print('The value of x is ' + str(x) + '.')
+
+def DeadFunction():
+    print('I am not being used.')
+if __name__ == '__main__':
+    n = int(sys.argv[1])
+    BranchCoverageDemo(n)
